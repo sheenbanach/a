021ffd5be1acdf565382937a07f893d014b3b0fc
Shintaro Daimon <daimon.fuji@gmail.com>
2017-07-27 06:55:45 +0000

    hjj

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :books
+  root 'coffee#homepage'
+
+  get 'starbucks/homepage'
+
+  get 'accessory/homepage'
+
+  get 'pages/homepage'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
